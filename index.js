423d4033de548485bdba528e87d335cc7ddde7ce
apilsrm <sharmaapil05@gmail.com>
2023-01-05 16:27:17 +0545

    basic javascript

---
+++ index.js
@@ -0,0 +1,53 @@
+// var x = 10;
+// console.log(x);
+// var x = 7 * 7 ;
+// console.log(x);
+// let y = 55;
+// console.log(y);
+// y = 56;
+// console.log(y);
+// let y = 558;
+// console.log(y); //y has been already declared
+
+// const PI = 3.14;
+// console.log(PI);
+// PI = 6.3;
+// console.log(PI); //Assignment to constant variable.
+// const PI = 33; //'PI' has already been declared 
+
+// const x = 90 ;
+// console.log(typeof x); //number
+// console.log(x);
+
+// let name = "apil";
+// console.log(typeof name);
+// console.log(name);
+
+// let bo = true;
+// console.log(typeof bo); // blooean
+// console.log(bo);
+
+// let x = 90;
+// let y = 60 ;
+// let z = true ;
+// let result = x + y + z;
+// console.log(typeof result); // 90 +60 +1
+// console.log(result);
+
+// let name = "apple ball";
+// let y = 50;
+// let find = name +" " + y ;
+// console.log(find);
+// console.log(typeof find);
+
+let fname = "Apil";
+let lname = "Sharma";
+// console.log("my name is : " + fname + " " + lname + ".");
+// console.log( `my name is : ${fname} ${lname} . `);
+
+const myGreeting = `my name is : ${fname} ${lname} . `;
+console.log (`Hello Everyone: ${myGreeting}`); 
+
+
+
+
